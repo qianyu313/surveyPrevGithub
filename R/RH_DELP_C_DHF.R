e20090d0beedd5cad48c33b00b813ab5145ad37e
qianyu313 <qianyu313@gmail.com>
2024-11-14 19:08:38 -0800

    CM_ECMR_C_NNF,allowing five year nmr in shinyapp

--- R/RH_DELP_C_DHF.R
+++ R/RH_DELP_C_DHF.R
@@ -1,4 +1,4 @@
-##'AH_TOBC_W_OTH
+##'RH_DELP_C_DHF
 #' @param Rdata  data.frame from survryPrev::getDHSdata
 #'
 #' @return A partially processed data.frame that will be used in survryPrev::getDHSindicator. The whole function can be used as a parameter in survryPrev::getDHSindicator
